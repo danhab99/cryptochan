3d31be442e0ec4505256ebd8898dfaf92b92b21a
Dan Habot <dan.habot@gmail.com>
2021-05-30 20:12:42 -0400

    tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    color: {},
+    extend: {
+      colors: {
+        background: colors.coolGray,
+      },
+    },
   },
   variants: {
     extend: {},
