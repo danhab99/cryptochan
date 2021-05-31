ea66fd80e33290e245afd78ea684277a4256c37e
Dan Habot <dan.habot@gmail.com>
2021-05-31 01:13:16 -0400

    basic entry component

---
+++ components/entry.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { IEntry } from "../schemas/Entry";
+
+interface EntryProps {
+  entry: IEntry;
+}
+
+const EntryComponent: React.FC<EntryProps> = ({ entry }) => {
+  return (
+    <div className="entryCard">
+      <p className="entryTitle">
+        <span className="font-bold">{entry.author.name}</span> (pk:
+        {entry.author.publickey}) {entry.published}
+        <span className="text-muted-600">
+          {"#"}
+          {entry.hash.value}
+        </span>
+      </p>
+
+      <p className="text-md">{entry.body.content}</p>
+    </div>
+  );
+};
+
+export default EntryComponent;
