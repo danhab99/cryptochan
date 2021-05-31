a491f4c93e25624a9f1b1a918c38db812f15e23f
Dan Habot <dan.habot@gmail.com>
2021-05-31 01:13:28 -0400

    Modified title

--- components/title.tsx
+++ components/title.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ThreadForm from "./threadform";
 
 interface TitleProps {}
 
 const Title: React.FC<TitleProps> = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <div>
       <header>
-        <h1>{process.env.TITLE}</h1>
+        <h1 className="text-primary-600">{process.env.TITLE}</h1>
+
+        <div>
+          <button onClick={() => setShowForm(true)}>Start a new thread</button>
+          {showForm ? (
+            <div>
+              <ThreadForm />
+            </div>
+          ) : null}
+        </div>
       </header>
     </div>
   );
