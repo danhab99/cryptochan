585f1199df7fbf38d02b729974694b39c297cd15
Dan Habot <dan.habot@gmail.com>
2021-06-05 13:11:48 -0400

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import React from "react";
 import "../styles/global.css";
 
-function MyApp({ Component, pageProps }) {
+interface MyAppProps {
+  Component: React.FC<any>;
+  pageProps: any;
+}
+
+function MyApp({ Component, pageProps }: MyAppProps) {
   return <Component {...pageProps} />;
 }
 
