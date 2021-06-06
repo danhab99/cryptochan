537c4fb0bd4af9a3e5aa82ba69a600d14d7bd9a6
Dan Habot <dan.habot@gmail.com>
2021-06-06 12:50:51 -0400

    public key registration

---
+++ pages/api/regkey.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse, PageConfig } from "next";
+import Busboy from "busboy";
+import * as openpgp from "openpgp";
+import connectDB from "../../middlewares/mongoose";
+import { PublicKey } from "../../schemas/PublicKey";
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await connectDB();
+
+  let busboy = new Busboy({
+    headers: req.headers,
+    limits: {
+      fileSize: 0,
+      fields: 1,
+    },
+  });
+
+  let armoredKey: string;
+
+  busboy.on("field", (fieldname: string, val: string) => {
+    if (fieldname === "publickey") {
+      armoredKey = val;
+    }
+  });
+
+  busboy.on("finish", () => {
+    debugger;
+    if (armoredKey.length) {
+      openpgp
+        .readKey({
+          armoredKey,
+        })
+        .then((key) => {
+          return key.getPrimaryUser().then((user) => ({ key, user }));
+        })
+        .then(({ key, user }) => {
+          let uid = user.user.userID;
+
+          PublicKey.create({
+            key: armoredKey,
+            fingerprint: key.getFingerprint(),
+            keyid: key.getKeyID().toHex(),
+            owner: {
+              name: uid?.name,
+              email: uid?.email,
+              userID: uid?.userID,
+              comment: uid?.comment,
+            },
+          }).then(() => {
+            res.writeHead(201, "added public key");
+            res.end();
+          });
+        });
+    } else {
+      res.writeHead(400);
+      res.end("field 'publickey' required");
+    }
+  });
+
+  req.pipe(busboy);
+};
