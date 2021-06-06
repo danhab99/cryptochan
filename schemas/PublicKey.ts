2d7da5974b6df53fe6473bb92a3ff119b3380602
Dan Habot <dan.habot@gmail.com>
2021-06-06 12:49:13 -0400

    public key schema

---
+++ schemas/PublicKey.ts
@@ -0,0 +1,32 @@
+import mongoose, { model, Schema, Model, Document } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  userID: string;
+  comment: string;
+}
+
+interface IPublicKey extends Document {
+  key: string;
+  fingerprint: string;
+  keyid: string;
+  owner: IUser;
+}
+
+const UserSchema: Schema = new Schema({
+  name: String,
+  email: String,
+  userID: String,
+  comment: String,
+});
+
+const PublicKeySchema: Schema = new Schema({
+  key: String,
+  fingerprint: String,
+  keyid: String,
+  owner: UserSchema,
+});
+
+export const PublicKey: Model<IPublicKey> =
+  mongoose.models.publickey || model("publickey", PublicKeySchema);
