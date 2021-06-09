617ae2aa39d7ef84f4a67162b9b2224a61315cfd
Dan Habot <dan.habot@gmail.com>
2021-06-09 15:35:20 -0400

    get public key

---
+++ pages/api/pk/[kid].ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "../../../middlewares/mongoose";
+import { PublicKey } from "../../../schemas/PublicKey";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await connectDB();
+
+  console.log("Getting key", req.query);
+
+  PublicKey.findOne({ keyid: req.query.kid as string }).then((pk) => {
+    if (pk) {
+      res.send(pk.key);
+    } else {
+      res.writeHead(404).end("Not found");
+    }
+  });
+};
