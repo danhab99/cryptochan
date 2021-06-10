d035f07dcc07d3292984d34bc9256eafd44fb160
Dan Habot <dan.habot@gmail.com>
2021-06-09 21:57:00 -0400

    fix

--- pages/api/pk/[kid].ts
+++ pages/api/pk/[kid].ts
@@ -7,14 +7,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   console.log("Getting key", req.query);
 
-  PublicKey.findOne({ keyid: req.query.kid as string }).then((pk) => {
-    if (pk) {
-      res.writeHead(200, {
-        "Cache-Control": "public, max-age=604800, immutable",
-      });
-      res.end(pk.key);
-    } else {
-      res.writeHead(404).end("Not found");
-    }
-  });
+  PublicKey.findOne({ keyid: req.query.kid as string })
+    .then((pk) => {
+      if (pk) {
+        res.writeHead(200, {
+          "Cache-Control": "public, max-age=604800, immutable",
+        });
+        res.end(pk.key);
+      } else {
+        res.writeHead(404).end("Not found");
+      }
+    })
+    .catch((e) => {
+      res.writeHead(500);
+      res.end(JSON.stringify(e));
+    });
 };
