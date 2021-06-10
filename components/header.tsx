c867475f2ad06521f2804f054aebe8af8a084784
Dan Habot <dan.habot@gmail.com>
2021-06-09 23:56:49 -0400

    fix

--- components/header.tsx
+++ components/header.tsx
@@ -6,21 +6,23 @@ type HeaderProps =
   | { type: "entry"; entry: IThreadSimple }
   | { type: "category"; category: string }
   | { type: "user"; user: any }
-  | { type: "error"; error: string };
+  | { type: "error"; error: string }
+  | { type: "nothing" };
 
 const PrefixMap: Record<HeaderProps["type"], string> = {
   category: "Category",
   entry: "Thread",
   error: "Error",
   user: "Public key",
+  nothing: "",
 };
 
 export const Header: React.FC<HeaderProps> = (props) => {
   return (
     <Head>
       <title>
-        {PrefixMap[props.type]} {props.type === "error" ? props.error : null} -{" "}
-        {process.env.TITLE}
+        {PrefixMap[props.type]} {props.type === "error" ? props.error : null}{" "}
+        {props.type !== "nothing" ? "-" : ""} {process.env.TITLE}
       </title>
     </Head>
   );
