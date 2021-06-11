5fcc046ef9b6384b517262599d6c896ad5be7ec4
Dan Habot <dan.habot@gmail.com>
2021-06-11 13:57:15 -0400

    cache control

--- pages/api/policy.ts
+++ pages/api/policy.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Policy } from "../../policy";
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader("Cache-Control", "public, max-age=604800, immutable");
   res.json(Policy);
 };
