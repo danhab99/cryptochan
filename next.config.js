c6226ddec52e201650a4759b411c12df85932eed
Dan Habot <dan.habot@gmail.com>
2021-06-11 13:38:11 -0400

    refactored environment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 // next.config.js
+require("dotenv").config();
 module.exports = {
   webpack(config, options) {
     return config;
   },
-  env: {
-    TITLE: "Cryptochan",
-  },
+  env: process.env,
   future: {
     webpack5: true,
   },
