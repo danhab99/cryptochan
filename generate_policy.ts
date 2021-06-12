02fef2f722811f797ae4744294ff6a2f421d9ca8
Dan Habot <dan.habot@gmail.com>
2021-06-11 23:26:18 -0400

    new way to inject policy

---
+++ generate_policy.ts
@@ -0,0 +1,30 @@
+import { IPolicy } from "./IPolicy";
+
+const policy: IPolicy = {
+  approve_posts: true,
+  publickey: {
+    preapproved: true,
+    require: true,
+  },
+  embeds: ["image/webp", "video/webm"],
+  maxEmbeds: 3,
+  maxSize: 1e7,
+  rules: [
+    "Posting is limited to only those with approved public keys",
+    "No porn",
+    "No gore",
+    "No death",
+    "No doxxing",
+  ],
+  categories: [
+    {
+      name: "all",
+      title: "All",
+      description: "Welcomes all posts",
+    },
+  ],
+  hash_algo: "SHA-256",
+  maxLength: 1e6,
+};
+
+console.log(JSON.stringify(policy));
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 const cleanEnv = require("dotenv").config().parsed;
 
 module.exports = {
-  env: _.pick(cleanEnv, ["TITLE"]),
+  env: _.pick(cleanEnv, ["TITLE", "POLICY"]),
   future: {
     webpack5: true,
   },
