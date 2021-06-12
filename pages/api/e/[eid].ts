af849d1d725a1e78d35ba3d40379969fcbf12c27
Dan Habot <dan.habot@gmail.com>
2021-06-11 22:12:03 -0400

    get the embed from s3 idk

--- pages/api/e/[eid].ts
+++ pages/api/e/[eid].ts
@@ -1,21 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
 import { evalFilename } from "../evalFilename";
+import { minioClient } from "../../../middlewares/minio";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   let p = evalFilename(req.query.eid as string);
 
-  if (fs.existsSync(p)) {
-    try {
-      res.writeHead(200, {
-        "Cache-Control": "public, max-age=604800, immutable",
-      });
-      let read = fs.createReadStream(p);
-      read.pipe(res);
-    } catch (e) {
-      res.status(500).json(e);
-    }
-  } else {
-    res.status(404).json(new Error("Not found"));
+  if (!req.query.eid) {
+    res.status(400).json(new Error("Embed hash required"));
+    return;
   }
+
+  minioClient.presignedGetObject(
+    (process.env.S3_PREFIX as string) + "-embeds",
+    req.query.eid as string,
+    30,
+    {
+      "Cache-Control": "public, max-age=604800, immutable",
+    },
+    (err, url) => {
+      console.log(err, url);
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.redirect(url);
+      }
+    }
+  );
 };
