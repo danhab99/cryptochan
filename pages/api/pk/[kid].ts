986089d33f2fd34cd31acd28abb861b86cc4f2f3
Dan Habot <dan.habot@gmail.com>
2021-06-11 23:59:12 -0400

    cleanup

--- pages/api/pk/[kid].ts
+++ pages/api/pk/[kid].ts
@@ -5,8 +5,6 @@ import { PublicKey } from "../../../schemas/PublicKey";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await connectDB();
 
-  console.log("Getting key", req.query);
-
   PublicKey.findOne({ keyid: req.query.kid as string })
     .then((pk) => {
       if (pk) {
