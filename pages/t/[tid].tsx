6fc519eeaf2c196f8787d0762164f20cc58cd5ae
Dan Habot <dan.habot@gmail.com>
2021-06-12 00:19:43 -0400

    added load more

--- pages/t/[tid].tsx
+++ pages/t/[tid].tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps, GetServerSidePropsResult } from "next";
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Header } from "../../components/header";
 import _ from "lodash";
 import { Thread, IThread } from "../../schemas/Thread";
@@ -16,9 +16,37 @@ interface ThreadPageProps {
   thread: IThread;
   parent?: IThread | null;
   replies?: Array<IThread>;
+  startPage: number;
+  more: boolean;
+  hash: string;
 }
 
 const ThreadPage: React.FC<ThreadPageProps> = (props) => {
+  const page = useRef(props.startPage);
+  const [loading, setLoading] = useState(false);
+  const [replies, setReplies] = useState(props.replies);
+  const [more, setMore] = useState(props.more);
+
+  const loadMore = async () => {
+    page.current += 1;
+    setLoading(true);
+    let resp = await fetch(`/api/t/${props.hash}?page=${page.current}`);
+
+    if (resp.ok) {
+      let threads: Array<IThread>;
+      let moreAvaliable: boolean;
+
+      ({ threads, moreAvaliable } = await resp.json());
+
+      setReplies((prev) => (prev ? prev.concat(threads) : threads));
+      setMore(moreAvaliable);
+    } else {
+      alert("Unable to fetch more");
+      console.error(await resp.text());
+    }
+    setLoading(false);
+  };
+
   return (
     <div>
       <Header type="entry" entry={props.thread} />
@@ -33,11 +61,22 @@ const ThreadPage: React.FC<ThreadPageProps> = (props) => {
 
       <ThreadComponent entry={props.thread} />
 
-      {props.replies ? (
+      {replies ? (
         <div className="replyBlock">
-          {props.replies.map((r) => (
+          {replies?.map((r) => (
             <ThreadComponent entry={r} />
           ))}
+        </div>
+      ) : null}
+
+      {more ? (
+        <div>
+          <h3
+            className="text-primary-700 text-center underline"
+            onClick={() => loadMore()}
+          >
+            [Load more{loading ? "..." : ""}]
+          </h3>
         </div>
       ) : null}
     </div>
@@ -46,12 +85,16 @@ const ThreadPage: React.FC<ThreadPageProps> = (props) => {
 
 export default ThreadPage;
 
+const PAGE_COUNT = 30;
+
 export const getServerSideProps: GetServerSideProps<ThreadPageProps> = async ({
   params,
+  query,
 }): Promise<GetServerSidePropsResult<ThreadPageProps>> => {
   await connectDB();
   try {
     const tid = sanatizeParams(params?.tid);
+    const page = parseInt((query.page as string) || "0");
 
     const thread = await sanatizeDB(Thread.findOne({ "hash.value": tid }));
 
@@ -61,8 +104,11 @@ export const getServerSideProps: GetServerSideProps<ThreadPageProps> = async ({
       };
     }
 
-    const replies = sanatizeDB(
-      Thread.find({ parenthash: tid }).sort({ published: -1 })
+    const replies = await sanatizeDB(
+      Thread.find({ parenthash: tid })
+        .sort({ published: -1 })
+        .skip(page * PAGE_COUNT)
+        .limit(PAGE_COUNT)
     );
 
     if (thread?.parenthash) {
@@ -74,7 +120,9 @@ export const getServerSideProps: GetServerSideProps<ThreadPageProps> = async ({
         props: {
           thread,
           parent: await parent,
-          replies: await replies,
+          replies,
+          more: replies.length >= PAGE_COUNT,
+          startPage: page,
         },
       };
     }
