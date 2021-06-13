80957f9fbc259cf1542bff4cb0d63b9874b6fa3b
Dan Habot <dan.habot@gmail.com>
2021-06-13 15:21:12 -0400

    fix

--- pages/api/c/[cat].ts
+++ pages/api/c/[cat].ts
@@ -1,23 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import connectDB from "../../../middlewares/mongoose";
-import { getThreadsInCategory } from "../../../query/getThreadsInCategory";
+import { getThreadsAndReplies } from "../../../query/getThreadsAndReplies";
+import { sanatizeParams } from "../../../sanatizeQuery";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   await connectDB();
-  console.log(req.query);
+  const cat = sanatizeParams(req.query.cat);
 
-  if (!req.query.cat) {
+  if (!cat) {
     res.status(406).json(new Error("Unknow category"));
     return;
   }
 
-  const { threadsAndReplies, threads } = await getThreadsInCategory(
-    req.query.cat as string,
+  const { threadsAndReplies, more } = await getThreadsAndReplies(
+    { type: "category", category: cat },
     {
       page: parseInt(req.query.page as string) || 0,
-      sort: "date",
     }
   );
 
-  res.json({ threads: threadsAndReplies, moreAvaliable: threads.length > 0 });
+  res.json({ threads: threadsAndReplies, moreAvaliable: more });
 };
