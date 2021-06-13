cd5ceef3411260630c9f30d9b6f4d1b883c50e31
Dan Habot <dan.habot@gmail.com>
2021-06-13 16:59:44 -0400

    Dynamic error message

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Title from "../components/title";
 import { Header } from "../components/header";
-import { useRouter } from "next/router";
 
 const Error404Page: React.FC = () => {
-  const router = useRouter();
-  console.log(router);
+  const [message, setMessage] = useState("404 Page not found");
+
+  useEffect(() => {
+    let s = window.location.pathname.split("/");
+    switch (s[1]) {
+      case "t":
+        setMessage("Thread not found or awaiting approval");
+        break;
+      case "pk":
+        setMessage("Public key not found");
+        break;
+    }
+  }, []);
+
   return (
     <div>
       <Title newThreads={false} />
       <Header prefix="404" />
 
       <header>
-        <h1 className="text-6xl">404 Page not found</h1>
+        <h1 className="text-6xl">{message}</h1>
       </header>
     </div>
   );
