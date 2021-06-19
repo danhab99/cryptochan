4bf5ffcb6535776f23e9cfb5892f7bbd254136d1
Dan Habot <dan.habot@gmail.com>
2021-06-19 18:52:59 -0400

    fix

--- pages/keys/index.tsx
+++ pages/keys/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../../components/title";
 import { Header } from "../../components/header";
-import { Link } from "next/link";
+import Link from "next/link";
 
 const Keys: React.FC = () => {
   return (
